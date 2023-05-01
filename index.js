e796648b3f875b17830ed729b0cbf7c718a0d6d4
AliaksandrButsin <aliaksandrbutsin@gmail.com>
2023-05-01 12:38:14 +0200

    add: button animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ========================================================= html =================================================================
 const section = document.createElement('section');
 section.classList.add('section_wrapper');
 document.body.prepend(section);
@@ -23,81 +24,82 @@ section.append(keyboardWrapper);
 
 keyboardWrapper.innerHTML = `
 <div class="line">
-    <div class="key symb mark lang" >\`</div>
-    <div class="key symb mark"      >1</div>
-    <div class="key symb mark"      >2</div>
-    <div class="key symb mark"      >3</div>
-    <div class="key symb mark"      >4</div>
-    <div class="key symb mark"      >5</div>
-    <div class="key symb mark"      >6</div>
-    <div class="key symb mark"      >7</div>
-    <div class="key symb mark"      >8</div>
-    <div class="key symb mark"      >9</div>
-    <div class="key symb mark"      >0</div>
-    <div class="key symb mark"      >-</div>
-    <div class="key symb mark"      >=</div>
-    <div class="key delete"         >delete</div>
+    <div class="key symb mark lang" id="Backquote">\`</div>
+    <div class="key symb mark"      id="Digit1">1</div>
+    <div class="key symb mark"      id="Digit2">2</div>
+    <div class="key symb mark"      id="Digit3">3</div>
+    <div class="key symb mark"      id="Digit4">4</div>
+    <div class="key symb mark"      id="Digit5">5</div>
+    <div class="key symb mark"      id="Digit6">6</div>
+    <div class="key symb mark"      id="Digit7">7</div>
+    <div class="key symb mark"      id="Digit8">8</div>
+    <div class="key symb mark"      id="Digit9">9</div>
+    <div class="key symb mark"      id="Digit0">0</div>
+    <div class="key symb mark"      id="Minus">-</div>
+    <div class="key symb mark"      id="Equal">=</div>
+    <div class="key delete"         id="Backspace">delete</div>
   </div>
   <div class="line">
-    <div class="key tab"            >tab</div>
-    <div class="key symb lett lang" >q</div>
-    <div class="key symb lett lang" >w</div>
-    <div class="key symb lett lang" >e</div>
-    <div class="key symb lett lang" >r</div>
-    <div class="key symb lett lang" >t</div>
-    <div class="key symb lett lang" >y</div>
-    <div class="key symb lett lang" >u</div>
-    <div class="key symb lett lang" >i</div>
-    <div class="key symb lett lang" >o</div>
-    <div class="key symb lett lang" >p</div>
-    <div class="key symb mark lang" >[</div>
-    <div class="key symb mark lang" >]</div>
-    <div class="key symb mark"      >\\</div>
+    <div class="key tab"            id="Tab">tab</div>
+    <div class="key symb lett lang" id="KeyQ">q</div>
+    <div class="key symb lett lang" id="KeyW">w</div>
+    <div class="key symb lett lang" id="KeyE">e</div>
+    <div class="key symb lett lang" id="KeyR">r</div>
+    <div class="key symb lett lang" id="KeyT">t</div>
+    <div class="key symb lett lang" id="KeyY">y</div>
+    <div class="key symb lett lang" id="KeyU">u</div>
+    <div class="key symb lett lang" id="KeyI">i</div>
+    <div class="key symb lett lang" id="KeyO">o</div>
+    <div class="key symb lett lang" id="KeyP">p</div>
+    <div class="key symb mark lang" id="BracketLeft">[</div>
+    <div class="key symb mark lang" id="BracketRight">]</div>
+    <div class="key symb mark"      id="Backslash">\\</div>
   </div>
   <div class="line">
-    <div class="key caps"           >caps lock</div>
-    <div class="key symb lett lang" >a</div>
-    <div class="key symb lett lang" >s</div>
-    <div class="key symb lett lang" >d</div>
-    <div class="key symb lett lang" >f</div>
-    <div class="key symb lett lang" >g</div>
-    <div class="key symb lett lang" >h</div>
-    <div class="key symb lett lang" >j</div>
-    <div class="key symb lett lang" >k</div>
-    <div class="key symb lett lang" >l</div>
-    <div class="key symb mark lang" >;</div>
-    <div class="key symb mark lang" >'</div>
-    <div class="key return"         >return</div>
+    <div class="key caps"           id="CapsLock">caps lock</div>
+    <div class="key symb lett lang" id="KeyA">a</div>
+    <div class="key symb lett lang" id="KeyS">s</div>
+    <div class="key symb lett lang" id="KeyD">d</div>
+    <div class="key symb lett lang" id="KeyF">f</div>
+    <div class="key symb lett lang" id="KeyG">g</div>
+    <div class="key symb lett lang" id="KeyH">h</div>
+    <div class="key symb lett lang" id="KeyJ">j</div>
+    <div class="key symb lett lang" id="KeyK">k</div>
+    <div class="key symb lett lang" id="KeyL">l</div>
+    <div class="key symb mark lang" id="Semicolon">;</div>
+    <div class="key symb mark lang" id="Quote">'</div>
+    <div class="key return"         id="Enter">return</div>
   </div>
   <div class="line">
-    <div class="key l_shift"        >shift</div>
-    <div class="key symb lett lang" >z</div>
-    <div class="key symb lett lang" >x</div>
-    <div class="key symb lett lang" >c</div>
-    <div class="key symb lett lang" >v</div>
-    <div class="key symb lett lang" >b</div>
-    <div class="key symb lett lang" >n</div>
-    <div class="key symb lett lang" >m</div>
-    <div class="key symb mark lang" >,</div>
-    <div class="key symb mark lang" >.</div>
-    <div class="key symb mark lang" >/</div>
-    <div class="key r_shift"        >shift</div>
+    <div class="key l_shift"        id="ShiftLeft">shift</div>
+    <div class="key symb lett lang" id="KeyZ">z</div>
+    <div class="key symb lett lang" id="KeyX">x</div>
+    <div class="key symb lett lang" id="KeyC">c</div>
+    <div class="key symb lett lang" id="KeyV">v</div>
+    <div class="key symb lett lang" id="KeyB">b</div>
+    <div class="key symb lett lang" id="KeyN">n</div>
+    <div class="key symb lett lang" id="KeyM">m</div>
+    <div class="key symb mark lang" id="Comma">,</div>
+    <div class="key symb mark lang" id="Period">.</div>
+    <div class="key symb mark lang" id="Slash">/</div>
+    <div class="key r_shift"        id="ShiftRight">shift</div>
   </div>
   <div class="line">
-    <div class="key" >cont</div>
-    <div class="key" >l_opti</div>
-    <div class="key" >l_comm</div>
-    <div class="key space" ></div>
-    <div class="key" >r_comm</div>
-    <div class="key" >r_opti</div>
+    <div class="key" id="ControlLeft">cont</div>
+    <div class="key" id="AltLeft">l_opti</div>
+    <div class="key" id="MetaLeft">comm</div>
+    <div class="key" id="Space"></div>
+    <div class="key" id="MetaRight">r_comm</div>
+    <div class="key" id="AltRight">opti</div>
 
-    <div class="  key symb arrow l">◀</div>
+    <div class="  key symb arrow l" id="ArrowLeft">◀</div>
     <div class="blind_box">
-      <div class="key symb arrow u">▲</div>
-      <div class="key symb arrow d">▼</div>
+      <div class="key symb arrow u"   id="ArrowUp">▲</div>
+      <div class="key symb arrow d" id="ArrowDown">▼</div>
     </div>
-    <div class="  key symb arrow r">▶</div>`;
-
+    <div class="  key symb arrow r" id="ArrowRight">▶</div>`;
+// ========================================================= html =================================================================
+// ========================================================= symbol =================================================================
 const keys = document.querySelectorAll('.key');
 const textArea = document.querySelector('.section_text');
 let capsLock = 0;
@@ -113,7 +115,25 @@ const symbols       = document.querySelectorAll('.mark');
 const langSymbols   = document.querySelectorAll('.lang');
 const letters       = document.querySelectorAll('.lett');
 const toushSymbols  = document.querySelectorAll('.symb');
-
+// ========================================================= symbol =================================================================
+// ========================================================= active + =================================================================
+document.addEventListener('keydown', (event) => {
+  console.log(event);
+  keys.forEach(el => {
+     if (el.id == event.code) {
+        el.classList.add('active')
+     }
+  })
+});
+document.addEventListener('keyup', (event) => {
+  keys.forEach(el => {
+     if (el.id == event.code) {
+        el.classList.remove('active')
+     } 
+  })
+})
+// ========================================================= active + =================================================================
+// ========================================================= chek lang =================================================================
 const getStartedPage = () => {
   if (localStorage.lang === 'undefined' || localStorage.lang === 'en') {
       localStorage.lang = 'en'
@@ -127,7 +147,8 @@ const getStartedPage = () => {
   }
 }
 getStartedPage()
-
+// ========================================================= chek lang =================================================================
+// ========================================================= change lang ===============================================================
 let ctrlAcrive = 0;
 let alrActive = 0;
 document.addEventListener('keydown', (event) => {
@@ -159,14 +180,14 @@ document.addEventListener('keyup', (event) => {
 function getLangTumbler() {
   if (alrActive === 1 && ctrlAcrive === 1) {
      if (localStorage.lang === 'en') {
-        localStorage.lang = 'rus'
+        localStorage.lang = 'ru'
      } else {
         localStorage.lang = 'en'
      }
   }
 }
 function getLangKeys() {
-  if (localStorage.lang === 'rus') {
+  if (localStorage.lang === 'ru') {
      for (let i = 0; i < langSymbols.length; i++){
         langSymbols[i].innerHTML = symbolsRu[i]
      }
@@ -177,3 +198,4 @@ function getLangKeys() {
      }
   }
 }
+// ========================================================= change lang =================================================================
